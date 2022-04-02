6e48e9b1165a4361ce0a5940ec110088d50665e4
SabrinaJackson1022 <SJackson@sf.edu>
2022-04-01 22:07:36 -0400

    Add project files.

---
+++ frmBMICalc.Designer.cs
@@ -0,0 +1,350 @@
+﻿
+namespace BMI_Calculator
+{
+    partial class frmBMICalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBMICalc));
+            this.lblPromo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhoneAndAddress = new System.Windows.Forms.Label();
+            this.lblRoundMeasurements = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblFeet = new System.Windows.Forms.Label();
+            this.lblInches = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.grpbxMeasurements = new System.Windows.Forms.GroupBox();
+            this.rdbtnMetric = new System.Windows.Forms.RadioButton();
+            this.rdbtnEnglishMeasurements = new System.Windows.Forms.RadioButton();
+            this.btnPhoneAndAddress = new System.Windows.Forms.Button();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblLBS = new System.Windows.Forms.Label();
+            this.lblYourBMI = new System.Windows.Forms.Label();
+            this.lblRecommendedBMI = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.picboxLogo = new System.Windows.Forms.PictureBox();
+            this.grpbxMeasurements.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPromo
+            // 
+            this.lblPromo.AutoSize = true;
+            this.lblPromo.Location = new System.Drawing.Point(132, 9);
+            this.lblPromo.Name = "lblPromo";
+            this.lblPromo.Size = new System.Drawing.Size(401, 17);
+            this.lblPromo.TabIndex = 0;
+            this.lblPromo.Text = "This BMI calculator is provided by IWishICouldLoseWeight.com";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(132, 68);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 17);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "*Name:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(387, 70);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 17);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "*Email:";
+            // 
+            // lblPhoneAndAddress
+            // 
+            this.lblPhoneAndAddress.Location = new System.Drawing.Point(12, 117);
+            this.lblPhoneAndAddress.Name = "lblPhoneAndAddress";
+            this.lblPhoneAndAddress.Size = new System.Drawing.Size(426, 45);
+            this.lblPhoneAndAddress.TabIndex = 3;
+            this.lblPhoneAndAddress.Text = "Help up help you stay healthy by entering your phone number and address so we can" +
+    " stay in contact with you:";
+            // 
+            // lblRoundMeasurements
+            // 
+            this.lblRoundMeasurements.AutoSize = true;
+            this.lblRoundMeasurements.ForeColor = System.Drawing.Color.Red;
+            this.lblRoundMeasurements.Location = new System.Drawing.Point(16, 189);
+            this.lblRoundMeasurements.Name = "lblRoundMeasurements";
+            this.lblRoundMeasurements.Size = new System.Drawing.Size(391, 17);
+            this.lblRoundMeasurements.TabIndex = 4;
+            this.lblRoundMeasurements.Text = "Please round all measurements to the nearest whole number";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(16, 231);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(54, 17);
+            this.lblHeight.TabIndex = 5;
+            this.lblHeight.Text = "*Height";
+            // 
+            // lblFeet
+            // 
+            this.lblFeet.AutoSize = true;
+            this.lblFeet.Location = new System.Drawing.Point(169, 233);
+            this.lblFeet.Name = "lblFeet";
+            this.lblFeet.Size = new System.Drawing.Size(20, 17);
+            this.lblFeet.TabIndex = 6;
+            this.lblFeet.Text = "ft.";
+            // 
+            // lblInches
+            // 
+            this.lblInches.AutoSize = true;
+            this.lblInches.Location = new System.Drawing.Point(321, 233);
+            this.lblInches.Name = "lblInches";
+            this.lblInches.Size = new System.Drawing.Size(23, 17);
+            this.lblInches.TabIndex = 7;
+            this.lblInches.Text = "in.";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(192, 65);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 22);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(444, 65);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(213, 22);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(215, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(76, 228);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(87, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(79, 267);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(84, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(128, 316);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(156, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(190, 361);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(156, 22);
+            this.textBox7.TabIndex = 16;
+            // 
+            // grpbxMeasurements
+            // 
+            this.grpbxMeasurements.Controls.Add(this.rdbtnMetric);
+            this.grpbxMeasurements.Controls.Add(this.rdbtnEnglishMeasurements);
+            this.grpbxMeasurements.Location = new System.Drawing.Point(467, 189);
+            this.grpbxMeasurements.Name = "grpbxMeasurements";
+            this.grpbxMeasurements.Size = new System.Drawing.Size(190, 136);
+            this.grpbxMeasurements.TabIndex = 17;
+            this.grpbxMeasurements.TabStop = false;
+            this.grpbxMeasurements.Text = "Measurement Options:";
+            // 
+            // rdbtnMetric
+            // 
+            this.rdbtnMetric.AutoSize = true;
+            this.rdbtnMetric.Location = new System.Drawing.Point(6, 45);
+            this.rdbtnMetric.Name = "rdbtnMetric";
+            this.rdbtnMetric.Size = new System.Drawing.Size(164, 21);
+            this.rdbtnMetric.TabIndex = 18;
+            this.rdbtnMetric.TabStop = true;
+            this.rdbtnMetric.Text = "Metric Measurements";
+            this.rdbtnMetric.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnEnglishMeasurements
+            // 
+            this.rdbtnEnglishMeasurements.AutoSize = true;
+            this.rdbtnEnglishMeasurements.Location = new System.Drawing.Point(6, 85);
+            this.rdbtnEnglishMeasurements.Name = "rdbtnEnglishMeasurements";
+            this.rdbtnEnglishMeasurements.Size = new System.Drawing.Size(172, 21);
+            this.rdbtnEnglishMeasurements.TabIndex = 19;
+            this.rdbtnEnglishMeasurements.TabStop = true;
+            this.rdbtnEnglishMeasurements.Text = "English Measurements";
+            this.rdbtnEnglishMeasurements.UseVisualStyleBackColor = true;
+            // 
+            // btnPhoneAndAddress
+            // 
+            this.btnPhoneAndAddress.Location = new System.Drawing.Point(493, 117);
+            this.btnPhoneAndAddress.Name = "btnPhoneAndAddress";
+            this.btnPhoneAndAddress.Size = new System.Drawing.Size(127, 45);
+            this.btnPhoneAndAddress.TabIndex = 18;
+            this.btnPhoneAndAddress.Text = "Add more contact information";
+            this.btnPhoneAndAddress.UseVisualStyleBackColor = true;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(16, 271);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(57, 17);
+            this.lblWeight.TabIndex = 19;
+            this.lblWeight.Text = "*Weight";
+            // 
+            // lblLBS
+            // 
+            this.lblLBS.AutoSize = true;
+            this.lblLBS.Location = new System.Drawing.Point(169, 272);
+            this.lblLBS.Name = "lblLBS";
+            this.lblLBS.Size = new System.Drawing.Size(30, 17);
+            this.lblLBS.TabIndex = 20;
+            this.lblLBS.Text = "lbs.";
+            // 
+            // lblYourBMI
+            // 
+            this.lblYourBMI.AutoSize = true;
+            this.lblYourBMI.Location = new System.Drawing.Point(16, 321);
+            this.lblYourBMI.Name = "lblYourBMI";
+            this.lblYourBMI.Size = new System.Drawing.Size(106, 17);
+            this.lblYourBMI.TabIndex = 21;
+            this.lblYourBMI.Text = "Your BMI Index:";
+            // 
+            // lblRecommendedBMI
+            // 
+            this.lblRecommendedBMI.AutoSize = true;
+            this.lblRecommendedBMI.Location = new System.Drawing.Point(16, 364);
+            this.lblRecommendedBMI.Name = "lblRecommendedBMI";
+            this.lblRecommendedBMI.Size = new System.Drawing.Size(168, 17);
+            this.lblRecommendedBMI.TabIndex = 22;
+            this.lblRecommendedBMI.Text = "Your Recommended BMI:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(499, 344);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(121, 57);
+            this.btnCalculate.TabIndex = 23;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // picboxLogo
+            // 
+            this.picboxLogo.ErrorImage = ((System.Drawing.Image)(resources.GetObject("picboxLogo.ErrorImage")));
+            this.picboxLogo.Image = ((System.Drawing.Image)(resources.GetObject("picboxLogo.Image")));
+            this.picboxLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("picboxLogo.InitialImage")));
+            this.picboxLogo.Location = new System.Drawing.Point(0, 0);
+            this.picboxLogo.Name = "picboxLogo";
+            this.picboxLogo.Size = new System.Drawing.Size(126, 104);
+            this.picboxLogo.TabIndex = 24;
+            this.picboxLogo.TabStop = false;
+            // 
+            // frmBMICalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(692, 413);
+            this.Controls.Add(this.picboxLogo);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblRecommendedBMI);
+            this.Controls.Add(this.lblYourBMI);
+            this.Controls.Add(this.lblLBS);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.btnPhoneAndAddress);
+            this.Controls.Add(this.grpbxMeasurements);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblInches);
+            this.Controls.Add(this.lblFeet);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblRoundMeasurements);
+            this.Controls.Add(this.lblPhoneAndAddress);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblPromo);
+            this.Name = "frmBMICalc";
+            this.Text = "BMI Calculator";
+            this.grpbxMeasurements.ResumeLayout(false);
+            this.grpbxMeasurements.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPromo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhoneAndAddress;
+        private System.Windows.Forms.Label lblRoundMeasurements;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblFeet;
+        private System.Windows.Forms.Label lblInches;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.GroupBox grpbxMeasurements;
+        private System.Windows.Forms.RadioButton rdbtnMetric;
+        private System.Windows.Forms.RadioButton rdbtnEnglishMeasurements;
+        private System.Windows.Forms.Button btnPhoneAndAddress;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblLBS;
+        private System.Windows.Forms.Label lblYourBMI;
+        private System.Windows.Forms.Label lblRecommendedBMI;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.PictureBox picboxLogo;
+    }
+}
+
